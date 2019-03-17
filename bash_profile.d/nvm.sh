3e99ed272de9281f99fa6c47a6a2422d7017cdc3
Diego Guerra <dgoguerra.or@gmail.com>
2019-03-17 15:25:58 +0100

    Configure NVM bash completion

--- bash_profile.d/nvm.sh
+++ bash_profile.d/nvm.sh
@@ -5,3 +5,7 @@ export NVM_DIR="/Users/dgo/.nvm"
 if [ -s "$NVM_DIR/nvm.sh" ]; then
     source "$NVM_DIR/nvm.sh"
 fi
+
+if [ -s "$NVM_DIR/bash_completion" ]; then
+    source "$NVM_DIR/bash_completion"
+fi
