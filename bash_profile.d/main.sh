bd853287af4c5d9ca5a34fc64e18093a07112236
Diego Guerra <dgoguerra.or@gmail.com>
2019-03-25 08:31:15 +0100

    Source all profile scripts from a single file
    Instead of loading a lot of smaller files, for performance reasons.

--- bash_profile.d/main.sh
+++ bash_profile.d/main.sh
@@ -28,3 +28,67 @@ export PATH="$PATH:$HOME/.composer/vendor/bin"
 
 # Disable Ansible cowsay easter egg. See: https://github.com/ansible/ansible/issues/10530
 export ANSIBLE_NOCOWS=1
+
+# Bash history
+# --------------------------------
+
+# Ignore commands that start with spaces, and duplicates.
+export HISTCONTROL=ignorespace:ignoredups
+# When the shell exits, append to the history file instead of overwriting it
+shopt -s histappend
+
+# NVM (Node Version Manager)
+# --------------------------------
+#
+# See: https://github.com/creationix/nvm
+
+export NVM_DIR="$HOME/.nvm"
+
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    source "$NVM_DIR/nvm.sh"
+fi
+
+# if [ -s "$NVM_DIR/bash_completion" ]; then
+#     source "$NVM_DIR/bash_completion"
+# fi
+
+
+# Google Cloud SDK
+# --------------------------------
+#
+# See: https://cloud.google.com/sdk/docs/downloads-interactive
+
+# Update PATH
+if [ -f "$HOME/Documents/google-cloud-sdk/path.bash.inc" ]; then
+    source "$HOME/Documents/google-cloud-sdk/path.bash.inc"
+fi
+
+# Enable shell command completion for gcloud.
+if [ -f "$HOME/Documents/google-cloud-sdk/completion.bash.inc" ]; then
+    source "$HOME/Documents/google-cloud-sdk/completion.bash.inc"
+fi
+
+# Homebrew
+# --------------------------------
+#
+# Configure bash completion of tools installed via Homebrew, and
+# a custom git prompt. Requires installing the formula:
+#
+#   brew install bash-completion
+#
+# For the git completion and prompt to work, git needs to have been
+# installed with hombrew (so it exposes its bash completion script
+# in $(brew --prefix)/etc/bash_completion.d). Install it with:
+#
+#   brew install git
+#
+
+if [ -f $(brew --prefix)/etc/profile.d/bash_completion.sh ]; then
+    source $(brew --prefix)/etc/profile.d/bash_completion.sh
+fi
+
+# Enable git prompt, enable showing unstaged (*) and staged (+) changes
+# next to the branch name.
+# See: https://github.com/git/git/blob/master/contrib/completion/git-prompt.sh
+GIT_PS1_SHOWDIRTYSTATE=true
+PS1='[\u@mbp \w$(__git_ps1)]\$ '
--- loader.sh
+++ loader.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Load all bash_profile.d scripts. This script will run at the
-# start of all shells (since OSX loads .bash_profile always, not
-# only at the start of login shells as standard).
+# start of all shells (since OSX loads .bash_profile always,
+# not only at the start of login shells as standard).
 
 # Current file directory
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
