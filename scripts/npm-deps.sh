6e17e1be7f1f988a89e9d1e9f178646d076cff32
Diego Guerra <dgoguerra.or@gmail.com>
2019-04-21 15:02:52 +0200

    Add `git pr`, `git open` aliases

--- scripts/npm-deps.sh
+++ scripts/npm-deps.sh
@@ -2,3 +2,7 @@
 
 # Generate UUIDs from the CLI by running `uuid`.
 npm install -g uuid
+
+# CLI to open files and URLs, used in `git-dot-open`
+# and `git-dot-pr` to open Github in the browser.
+npm install -g opn-cli
