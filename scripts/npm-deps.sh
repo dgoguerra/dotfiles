4b1cbbbfa70b892ea7c204417e98d2204468cbfd
Diego Guerra <dgoguerra.or@gmail.com>
2019-04-26 11:32:46 +0200

    Add `which-cloud` CLI

--- scripts/npm-deps.sh
+++ scripts/npm-deps.sh
@@ -3,6 +3,9 @@
 # Generate UUIDs from the CLI by running `uuid`.
 npm install -g uuid
 
+# Find out cloud provider of an IP.
+npm install -g which-cloud
+
 # CLI to open files and URLs, used in `git-dot-open`
 # and `git-dot-pr` to open Github in the browser.
 npm install -g opn-cli
